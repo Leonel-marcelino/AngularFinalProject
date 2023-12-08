2d0aa765788fc75b4a0b4b925cb84517c72d7680
Leonel Marcelino <leonelmmarcelino@hotmail.com>
2023-12-08 15:46:04 +0000

    db

--- AngularFinalProject/Data/ApplicationDbContext.cs
+++ AngularFinalProject/Data/ApplicationDbContext.cs
@@ -5,11 +5,17 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace AngularFinalProject.Data
+
+
 {
-    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>    
     {
-        
-    public DbSet<Recipe> Recipes { get; set; }
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Ingredients> Ingredients { get; set; }
         public DbSet<Favorits> Favorites { get; set; }
