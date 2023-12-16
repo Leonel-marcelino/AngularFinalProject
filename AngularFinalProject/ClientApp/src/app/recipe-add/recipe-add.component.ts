248401df28f21f25a0b34020e04f0d8fa64ae5b3
Leonel Marcelino <leonelmmarcelino@hotmail.com>
2023-12-16 16:48:46 +0000

    #form-add-recipe

--- AngularFinalProject/ClientApp/src/app/recipe-add/recipe-add.component.ts
+++ AngularFinalProject/ClientApp/src/app/recipe-add/recipe-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorizeGuard } from '../../api-authorization/authorize.guard';
+import { RecipeService } from '../services/recipe.service'; 
 
 @Component({
   selector: 'app-recipe-add',
@@ -8,8 +9,30 @@ import { AuthorizeGuard } from '../../api-authorization/authorize.guard';
 })
 export class RecipeAddComponent implements OnInit {
 
-  constructor(private authorizeGuard: AuthorizeGuard) { }
+  constructor(private authorizeGuard: AuthorizeGuard, private recipeService: RecipeService) { }
+
+  recipe: any = {
+    name: '',
+    description: '',
+    title: '',
+    IsApproved: '', 
+    Difficulty: '',
+    Category: '',
+    Duration: ''
+  };
 
   ngOnInit(): void { }
 
+  onSubmit() {
+    // Chame o serviço para adicionar a receita
+    this.recipeService.addRecipe(this.recipe).subscribe(
+      (response) => {
+        console.log('Receita adicionada com sucesso:', response);
+        // Faça redirecionamento ou ações adicionais se necessário
+      },
+      (error) => {
+        console.error('Erro ao adicionar receita:', error);
+      }
+    );
+  }
 }
